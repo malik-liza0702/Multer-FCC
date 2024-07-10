5774d70c95b9cdf0f5059e92da90d8cd8e3dc905
malik-liza <malik.liza0702@gmail.com>
2024-07-10 14:12:43 +0530

    fileupload

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer=require("multer");
+const upload=multer();
+
+
 
 var app = express();
 
@@ -10,6 +14,16 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post("/api/fileanalyse",upload.single("upfile"),(req,res)=>{
+  res.json({
+    name:req.file.originalname,
+    type:req.file.mimetype,
+    size:req.file.size,
+  })
+   
+
+})
 
 
 
